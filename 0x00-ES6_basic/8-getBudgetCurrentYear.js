51242680e20ab53372f7747b5e527410366ca885
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-07-22 13:41:32 +0000

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,15 +1,14 @@
-//8-getBudgetCurrentYear.js
 function getCurrentYear() {
-	    const date = new Date();
-	    return date.getFullYear();
-	  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-	      const budget = {
-		            [`income-${getCurrentYear()}`]: income,
-		            [`gdp-${getCurrentYear()}`]: gdp,
-		            [`capita-${getCurrentYear()}`]: capita,
-		          };
-	    
-	      return budget;
-	    }
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
