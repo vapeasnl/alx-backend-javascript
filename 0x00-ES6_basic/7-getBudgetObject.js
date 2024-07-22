8a60309b7df45ec7ab1b5846844f0f0fb3b30577
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-07-22 13:40:56 +0000

    Update 7-getBudgetObject.js

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,9 @@
-//7-getBudgetObject.js
 export default function getBudgetObject(income, gdp, capita) {
-	    const budget = {
-		          income,
-		          gdp,
-		          capita,
-		        };
-	  
-	    return budget;
-	  }
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
