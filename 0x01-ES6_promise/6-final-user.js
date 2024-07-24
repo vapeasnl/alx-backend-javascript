accbda211eaabcb99cc0583d7171905709a19c3a
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-07-24 16:23:38 +0000

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,13 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const userPromise = signUpUser(firstName, lastName);
-    const photoPromise = uploadPhoto(fileName);
-
-    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
-
-    const userStatus = userResult.status === 'fulfilled' ? 'success' : 'failure';
-    const photoStatus = photoResult.status === 'fulfilled' ? 'success' : 'failure';
-
-    return [
-      { status: userStatus, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
-      { status: photoStatus, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason }
-    ];
-  } catch (error) {
-    throw new Error(error);
-  }
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
