6dc8e11a7eb75c885b216d0ca17cfa13c44d15bb
Abdessamad SALIM <samimalim1991@gmail.com>
2024-07-24 19:38:42 +0000

    Update 100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,15 @@
-import { uploadPhoto, createUser } from './utils';
+// 100-await.js
+import { uploadPhoto, createUser } from './utils.js';
 
-export default async function asyncUploadUser() {
-  let res = {};
-
+async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    res = { photo, user };
-  } catch (err) {
-    res = { photo: null, user: null };
+    return { photo, user };
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return { photo: null, user: null };
   }
-  return res;
 }
+
+export default asyncUploadUser;
