49dbffca73a8c4ca263972bafb455494bf0d6a2d
abdessamadsalim <samimalim1991@gmail.com>
2024-07-24 10:39:11 -0700

    task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,9 +1,15 @@
-import { uploadPhoto, createUser } from './utils';
+// 100-await.js
+import { uploadPhoto, createUser } from './utils.js';
 
-export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  return {
-    photo: { status: `${photo.status}`, body: `${photo.body}` },
-    user: { firstName: `${user.firstName}`, lastName: `${user.lastName}` },
-  };
+async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return { photo: null, user: null };
+  }
+}
+
+export default asyncUploadUser;
