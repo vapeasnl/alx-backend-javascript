4570e2b39a9f0a3d2605c15501e332714691ef7d
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-07-24 16:24:38 +0000

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.message);
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
   } finally {
     queue.push('Guardrail was processed');
   }
+
   return queue;
 }
