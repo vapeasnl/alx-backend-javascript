656a5f09c7deafd781aa80ebaae0cc13d57a6a33
Abdessamad SALIM <samimalim1991@gmail.com>
2024-07-25 11:50:16 +0000

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,22 +1,30 @@
+
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
+  set floors(floors) {
+    if (floors === undefined || floors === null) {
+      throw new Error('floors must be defined');
+    } else if (typeof floors === 'string') {
+      throw new Error('floors must be a number');
+    } else if (typeof floors !== 'number' || floors <= 0) {
+      throw new Error('floors must be a positive number');
+    } else if (!Number.isFinite(floors)) {
+      throw new Error('floors must be a finite number');
     }
-    this._floors = newFloors;
+    this._floors = Number(floors);
   }
 
+  // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
