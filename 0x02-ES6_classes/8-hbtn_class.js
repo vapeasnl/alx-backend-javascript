b88d2746b14748de33cce98614e2661304f89d1a
Abdessamad SALIM <samimalim1991@gmail.com>
2024-07-25 12:49:23 +0000

    upd spaces

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,13 +1,32 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-      this._size = size;
-      this._location = location;
-    }
-  
-    [Symbol.toPrimitive](hint) {
-      if (hint === 'number') {
-        return this._size;
-      }
-      return this._location;
-    }
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
   }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
