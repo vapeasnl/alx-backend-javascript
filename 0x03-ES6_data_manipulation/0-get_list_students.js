594089f8bcac1d8e08f3f231d5b649968425f0f5
Abdessamad SALIM <samimalim1991@gmail.com>
2024-07-30 11:48:02 +0000

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,24 +1,7 @@
 export default function getListStudents() {
-    const student1 = {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    };
-  
-    const student2 = {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    };
-  
-    const student3 = {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    };
-  
-    const arrStudents = [student1, student2, student3];
-  
-    return arrStudents;
-  }
-  
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
