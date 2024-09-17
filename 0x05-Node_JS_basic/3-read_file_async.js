05a33519880cc49b1e1ebf3efb204c5f78707110
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-09-17 10:14:10 +0200

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,44 +1,43 @@
 const fs = require('fs');
 
-
-const countStudents = (dataPath) => new Promise((resolve, reject) => {
-  fs.readFile(dataPath, 'utf-8', (err, data) => {
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
       reject(new Error('Cannot load the database'));
+      return;
     }
-    if (data) {
-      const fileLines = data
-        .toString('utf-8')
-        .trim()
-        .split('\n');
-      const studentGroups = {};
-      const dbFieldNames = fileLines[0].split(',');
-      const studentPropNames = dbFieldNames
-        .slice(0, dbFieldNames.length - 1);
 
-      for (const line of fileLines.slice(1)) {
-        const studentRecord = line.split(',');
-        const studentPropValues = studentRecord
-          .slice(0, studentRecord.length - 1);
-        const field = studentRecord[studentRecord.length - 1];
-        if (!Object.keys(studentGroups).includes(field)) {
-          studentGroups[field] = [];
+    // console.log('CSV file content:', data);
+
+    const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+    if (lines.length === 0) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    lines.shift();
+    const students = {};
+    let totalStudents = 0;
+
+    lines.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (firstname && field) {
+        if (!students[field]) {
+          students[field] = [];
         }
-        const studentEntries = studentPropNames
-          .map((propName, idx) => [propName, studentPropValues[idx]]);
-        studentGroups[field].push(Object.fromEntries(studentEntries));
+        students[field].push(firstname);
+        totalStudents += 1;
       }
+    });
 
-      const totalStudents = Object
-        .values(studentGroups)
-        .reduce((pre, cur) => (pre || []).length + cur.length);
-      console.log(`Number of students: ${totalStudents}`);
-      for (const [field, group] of Object.entries(studentGroups)) {
-        const studentNames = group.map((student) => student.firstname).join(', ');
-        console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    let result = `Number of students: ${totalStudents}`;
+    for (const field in students) {
+      if (Object.prototype.hasOwnProperty.call(students, field)) {
+        const list = students[field].join(', ');
+        result = `Number of students in ${field}: ${students[field].length}. List: ${list}`;
       }
-      resolve(true);
     }
+    resolve(result);
   });
 });
 
