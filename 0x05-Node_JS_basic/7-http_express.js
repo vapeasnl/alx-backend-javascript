caffad3d4184cf62ba4fe5aa42107b20b15f1ee3
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-09-17 10:38:35 +0200

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,19 +1,50 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
 const app = express();
+const PORT = 1245;
 
-app.get('/', (req, res) => res.send('Hello Holberton School!'));
-app.get('/students', async (req, res) => {
-  const initialHeaderResponse = 'This is the list of our students\n';
-  countStudents(process.argv[2])
-    .then((response) => {
-      const message = `${initialHeaderResponse}${response}`;
-      res.send(message);
-    })
-    .catch((err) => res.send(`${initialHeaderResponse}${err.message}`));
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.get('/students', (req, res) => {
+  const databaseFile = process.argv[2];
+
+  fs.readFile(databaseFile, 'utf8')
+    .then((data) => {
+      const students = data.trim().split('\n').filter((line) => line.trim() !== '');
+      const fields = {};
+
+      students.shift(); // Remove header line
+
+      students.forEach((student) => {
+        const [firstname, , , field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(`${firstname}`);
+      });
+
+      let response = `This is the list of our students\nNumber of students: ${students.length}\n`;
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+        }
+      }
+
+      res.type('text/plain');
+      res.send(response);
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      res.status(500).send('Cannot load the database');
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
 
 module.exports = app;
